23ccb204cedd8016ea910eed655f94ea83e049dc
fredahkioko <fredah.kioko@strathmore.edu>
2024-10-29 12:29:25 +0300

    minor edit

--- user_input.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-
-age = input("Enter your age: ")
-
-location = input("Enter your location: ")
-
-print (f"Hello {name}, you are {age} years old and live in {location}.")
